c32762848d47dc4f9f362adffc1ebc75d627229d
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-07 15:53:09 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, m, k; scanf("%ld %ld %ld", &n, &m, &k);
+        long x = (n /k);
+        long a = (x < m) ? x : m;
+        long b = ((m - a) + (k - 2)) / (k - 1);
+        long res = a - b;
+        printf("%ld\n", res);
+    }
+
+    return 0;
+}
